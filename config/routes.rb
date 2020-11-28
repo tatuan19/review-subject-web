20b841e455a0797cca2246719ce42f5c1678ecc3
Ubuntu <ubuntu@ip-172-31-29-60.ec2.internal>
2020-11-28 01:39:31 +0000

    add  navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :subject_files
-  # root 'static_pages#home'
-  root 'subject_files#index'
+  root 'static_pages#home'
+  # root 'subject_files#index'
   
   # get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
