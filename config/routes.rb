62f65527ba95126a0795af7999ffb4d2704c47e7
Ubuntu <ubuntu@ip-172-31-29-60.ec2.internal>
2020-11-28 05:58:50 +0000

    add bootstrap table subject

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   # root 'subject_files#index'
   
-  # get 'static_pages/home'
+   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
