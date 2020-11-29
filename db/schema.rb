f874ba2a0bba8cfa398880ccf3647e014bfee93d
NhuHa99 <onha30101999@gmail.com>
2020-11-29 15:45:20 +0000

    user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_050704) do
+ActiveRecord::Schema.define(version: 2020_11_29_142608) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2020_11_29_050704) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "subject_files", force: :cascade do |t|
     t.integer "subject_id"
     t.string "title"
@@ -29,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_11_29_050704) do
     t.string "file_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "file"
     t.integer "user_id"
   end
 
@@ -58,7 +68,10 @@ ActiveRecord::Schema.define(version: 2020_11_29_050704) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
