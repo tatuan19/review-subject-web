3c9131704ef9a7f7f3f2ddb249569ae1ce74b8d4
tatuan19 <trananhtuan12a10@gmail.com>
2020-11-30 04:18:07 +0000

    change root path + add file button

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   resources :subject_reviews
   resources :subjects
   resources :subject_files
-  root 'static_pages#home'
-  # root 'subject_files#index'
+#   root 'static_pages#home'
+   root 'subjects#index'
   
-   get 'static_pages/home'
+#   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
